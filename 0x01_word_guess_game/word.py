0708ab2625ccb03a593fb4782215e5879ec00ee4
Precious Yankson <maamearaba1612@gmail.com>
2023-10-13 06:25:01 -0700

    Edited the code

---
+++ 0x01_word_guess_game/word.py
@@ -0,0 +1,7 @@
+import random
+name_list = ["stanley", "praise", "abena", "kate", "nehemiah", "kristal",
+             "elijah", "ergesis", "kelvin", "stephen", "caleb", "skekwonya"]
+
+question_dict = {"what is the name": "henry", "what is age": "seen"}
+
+
