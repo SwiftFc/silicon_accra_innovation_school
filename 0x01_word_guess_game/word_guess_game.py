56da854df7904be627f4b512ca0321c16da3e5b6
SwiftFc <aseiduhenrydade@gmail.com>
2023-10-13 00:32:08 +0000

    new changes made to the game

--- 0x01_word_guess_game/word_guess_game.py
+++ 0x01_word_guess_game/word_guess_game.py
@@ -11,9 +11,20 @@ def welcome():
     print("Hello Silicon Accra\n")
     print("Welcome to the Word Guessing Game!!!\n")
 
+    print("*****************************************\n")
+    print("Answer the Python Question Below to get Access to the Game\n")
     
     while True:   
-        user_name = input("Enter Your Name to get started: ")
+        question = "What is the string method in python to add something to a list: "
+        user = input(question)
+        if user == ".append()":
+            print("\nYou are absolutely correct. *Answer = .append() ")
+            break
+        else:
+            print("\n******Keep Trying******\n*****Yes You Can*****\n*****You've Got This****")
+
+    while True:
+        user_name = input("\n\nEnter Your Name to get started: ")
 
         if user_name.replace(" ", "").isalpha() != True:
             print("\nInvalid ***  Expecting Alphabets only")
@@ -21,9 +32,22 @@ def welcome():
             print("\n****Login Successful💻****")
             print("**** Have fun playing and see if you can guess the hidden words!🎮🎮🎮****")
             break
+
+    while True:
+        print("\nAnswer this question to Now Start the game\n")
+        question1 = "\nHow do you append multiple values to a list python: "
+
+        user1 = input(question1)
+        if user1 == ".extend()":
+            print("***You answered correctly*** Answer = .extend() ")
+            break
+        else:
+            print("Keep Trying")
     
 name_list = ["stanley", "praise", "abena", "kate", "nehemiah", "kristal", 
              "elijah", "ergesis", "kelvin", "stephen", "caleb", "skekwonya"]
+
+
 
 
 def pick_random_word():             #defining a function to pick a random word
@@ -47,9 +71,9 @@ def start_game():
         while total_lives > 0:
             words_to_display = display_word(guess_a_word, guessed_letters)
             print("\nGuess the hidden word: " + words_to_display)
-            print("\n*****HINTS: NAMES OF STUDENT IN SAIS:🧩🧩*****")
+            print("\n*****HINTS: NAMES OF STUDENT IN SAIS:🧩🧩*****\n")
             print(f"*****You have {total_lives} lives left to guess****")
-        
+            print("*******************************")
             user_guess = input("Guess a letter🤔🤔: ").lower()
         
             if len(user_guess) == 1 and user_guess.isalpha(): #Here Both Conditions have to be true
@@ -72,9 +96,19 @@ def start_game():
                           \n****🏆🏆🏆🏆🏆🏆🏆🏆*****
                           \n*****Congratulations*****""")
                 break
+        question2 = "What special method allows you to initalize attributes in python: "
+
         if total_lives == 0:
             print("\nSorry, you're out of attempts. The word was: " + guess_a_word)
-            print("\n\n*********EXIT💔💔💔**************")
+            print("Answer the question to play again or exit.")
+            x = input(question2).strip()
+
+            if x.lower() == "def __init__":
+                print("\nCorrect answer! You may procede to play the game again")
+                start_game()
+            else:
+                print("\nGame has Ended")
+                print("\n\n*********EXIT💔💔💔**************")
             
 
 welcome()
