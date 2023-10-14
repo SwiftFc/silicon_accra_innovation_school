aa3fecafaebece48f9011453f7b59a381973f858
SwiftFc <aseiduhenrydade@gmail.com>
2023-10-14 03:18:44 +0000

    Added Random Questions To The Game

---
+++ 0x01_word_guess_game/word.py
@@ -0,0 +1,3 @@
+name_list = ["henry", "praise", "achiri", "kate", "nehemiah", "kristal",
+             "precious", "abena", "elijah", "kelvin", "stephen", "skekwonya",
+             "stanley", "ergesis", "caleb"]
--- 0x01_word_guess_game/word_guess_game.py
+++ 0x01_word_guess_game/word_guess_game.py
@@ -1,4 +1,5 @@
 import random
+from word import name_list
 
 """
 *Main -  Implementation of a Word Guessing Game in python
@@ -7,23 +8,62 @@ import random
          from a predefined list, and a player 
          is tasked to guess the letters in the word."""
 
+question_list = [
+    {"question": "Which of data type in python does not have the changeability characteristics:", "answer": "tuple"},
+    {"question": "How do you open a file in Python:", "answer": "open()"},
+    {"question": "Which data type in Python is unordered:", "answer": "dictionary"},
+    {"question": "Which keyword function returns the number of items in an object, such as a string or list:", "answer": "len()"},
+    {"question": "What keyword method in python is used to get the absolute value of a number:", "answer": "abs()"}]
+
+def select_random_question():
+    return random.choice(question_list)
+
+def ask_random_question():
+    random_question = select_random_question()
+    question_text = random_question["question"]
+    correct_answer = random_question["answer"]
+
+    while True:
+        user_answer = input(f"{question_text} ").strip()
+    
+        if user_answer.lower() == correct_answer.lower():
+            print(f"You are correct. **Answer = {correct_answer}\n")
+            break        
+        else:
+            print("******Wrong Answer❌❌❌\n*****Keep Trying****\n")
+            
+
+        
+        
 def welcome():
     print("Hello Silicon Accra\n")
-    print("Welcome to the Word Guessing Game!!!\n")
+    print("Welcome to the Word Guessing Game🚀🚀🚀!!!\n")
+
+    print("**********************************************\n")
+    print("Answer the Simple Python Question Below to get Access to the Program\n")
+
+    ask_random_question()
 
     
-    while True:   
-        user_name = input("Enter Your Name to get started: ")
+            
 
+    while True:
+        user_name = input("***************************************\nEnter Your Name to get started: ")
         if user_name.replace(" ", "").isalpha() != True:
-            print("\nInvalid ***  Expecting Alphabets only")
+            print("\nInvalid❌❌❌❌  Expecting Alphabets only")
         else:
-            print("\n****Login Successful💻****")
-            print("**** Have fun playing and see if you can guess the hidden words!🎮🎮🎮****")
+            desn = 'Login Successful💻💻💻'.center(50, '*')
+            print(f"\n{desn}")
+            print("**** Have fun🎉🎉 playing and see if you can guess the hidden words!🎮🎮🎮****\n")
             break
+            
+    print("\nAnswer three final questions to Start the game\n")
+    ask_random_question()
+    ask_random_question()
+    ask_random_question()
     
-name_list = ["stanley", "praise", "abena", "kate", "nehemiah", "kristal", 
-             "elijah", "ergesis", "kelvin", "stephen", "caleb", "skekwonya"]
+
+
 
 
 def pick_random_word():             #defining a function to pick a random word
@@ -46,20 +86,22 @@ def start_game():
 
         while total_lives > 0:
             words_to_display = display_word(guess_a_word, guessed_letters)
-            print("\nGuess the hidden word: " + words_to_display)
-            print("\n*****HINTS: NAMES OF STUDENT IN SAIS:🧩🧩*****")
-            print(f"*****You have {total_lives} lives left to guess****")
-        
+            design = 'WELCOME TO THE MAIN PAGE OF THE WORD GUESSING GAME'.center(100, '*')
+            print(design)
+            print("\nGUESS THE HIDDEN WORD:  " + words_to_display)
+            print("\n****  HINT: NAMES OF STUDENT IN SAIS 🧩🧩  *\n")
+            print(f"*****  You have {total_lives} lives left to guess  *****")
+            print("**************🧩🧩🧩🧩🧩🧩******************\n")
             user_guess = input("Guess a letter🤔🤔: ").lower()
         
             if len(user_guess) == 1 and user_guess.isalpha(): #Here Both Conditions have to be true
                 if user_guess in guessed_letters:
-                    print("**Letter Guessed Already")
+                    print("**Letter Guessed Already❌❌")
                 elif user_guess in guess_a_word:
-                    print("Correct Guess")
+                    print("Correct Guess👏👏✔✔")
                     guessed_letters.append(user_guess)
                 else:
-                    print("**Wrong Guess**, **Try again**")
+                    print("**Wrong Guess❌, **Try again**")
                     guessed_letters.append(user_guess)
                     total_lives -= 1
             else:
@@ -72,9 +114,20 @@ def start_game():
                           \n****🏆🏆🏆🏆🏆🏆🏆🏆*****
                           \n*****Congratulations*****""")
                 break
+        question2 = "What special method allows you to initalize attributes in python: "
+
         if total_lives == 0:
-            print("\nSorry, you're out of attempts. The word was: " + guess_a_word)
-            print("\n\n*********EXIT💔💔💔**************")
+            print("\nSorry, you're out of attempts😢😢😢. The word was: " + guess_a_word)
+            print("\n\nAnswer the question below to play again or exit🎉🎉🎉.\n")
+            x = input(question2).strip()
+
+            if x.lower() == "def __init__":
+                print("\nCorrect answer✔✔✔! You may proceed to play the game🎮🚀🚀🎮 again")
+                ask_random_question()
+                start_game()
+            else:
+                print("\n   Wrong Answer, Game has Ended")
+                print("\n*********EXIT💔💔💔**************")
             
 
 welcome()
